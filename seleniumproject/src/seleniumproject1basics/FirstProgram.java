4da77c5ea857cc476370230e5a182ccbdf0439b8
AnilArya10 <42232871+AnilArya10@users.noreply.github.com>
2018-09-12 11:40:44 +0530

    added comment

--- seleniumproject/src/seleniumproject1basics/FirstProgram.java
+++ seleniumproject/src/seleniumproject1basics/FirstProgram.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.WebDriver;
 public class FirstProgram {
 		public static void main(String[] args) {
 			Defaultselenium selenium=new Defaultselenium("localhost",4444,"firefox C:\\Program Files");
-			selenium.start();
+			selenium.start();//what is this  some thing wrong
 			
 		}
 
